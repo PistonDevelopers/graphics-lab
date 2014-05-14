ca6e367b7dc925380f342520deae0aef831b5ae3
Sven Nilsen <bvssvni@gmail.com>
2014-05-14 12:58:50 +0200

    Testing round line

--- src/line_app.rs
+++ src/line_app.rs
@@ -8,19 +8,9 @@ use glfw::{Key};
 
 pub struct App;
 
-fn line(x1: f64, y1: f64, x2: f64, y2: f64, width: f64, c: &ColorContext, gl: &mut Gl) {
-    let dx = x2 - x1;
-    let dy = y2 - y1;
-    let length = (dx * dx + dy * dy).sqrt();
-    c.trans_local(x1, y1)
-    .orient_local(x2 - x1, y2 - y1)
-    .rect(0.0, -0.5 * width, length, width)
-    .fill(gl);
-}
-
 impl Game for App {
     fn render(&self, c: &Context, gl: &mut Gl) {
-        line(0.0, 0.0, 0.5, 0.0, 0.04, &c.rgb(1.0, 0.0, 0.0).trans(0.5, 0.5), gl);
+        c.line(0.0, 0.0, 0.5, 0.0).round_border_radius(0.1).rgb(1.0, 0.0, 0.0).stroke(gl);
     }
 
     fn update(&mut self, _dt: f64, _asset_store: &mut AssetStore) {
