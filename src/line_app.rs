d2b2be524b0a973dfe1393801e63ab516e9ad1d5
Sven Nilsen <bvssvni@gmail.com>
2014-05-16 15:25:57 +0200

    Added line app test

---
+++ src/line_app.rs
@@ -0,0 +1,31 @@
+
+// Allow dead code since this is an example app.
+#![allow(dead_code)]
+
+use graphics::*;
+use piston::*;
+use {
+    GameWindowBackEnd,
+};
+
+pub struct App;
+
+fn return_test(c: &Context) -> BevelRectangleContext {
+    c.rect(0.0, 0.0, 0.5, 0.5).bevel(0.1).clone()
+}    
+
+impl Game<GameWindowBackEnd> for App {
+    fn render(&self, c: &Context, gl: &mut Gl) {
+        c.line(0.0, 0.0, 0.5, 0.5).square_border_radius(0.1).rgb(1.0, 0.0, 0.0).stroke(gl);
+        match return_test(c) {
+            c => c.rgb(0.0, 1.0, 0.0).fill(gl)
+        };
+    }
+}
+
+impl App {
+    pub fn new() -> App {
+        App
+    }
+}
+
