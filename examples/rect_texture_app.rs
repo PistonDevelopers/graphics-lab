5992527e611c4902635877b87149496aed4238f2
Sven Nilsen <bvssvni@gmail.com>
2014-06-26 16:15:33 +0200

    Added example for rectangle texture bug
    See https://github.com/PistonDevelopers/rust-graphics/issues/550

---
+++ examples/rect_texture_app.rs
@@ -0,0 +1,69 @@
+
+// Allow dead code since this is an example app.
+#![allow(dead_code)]
+#![feature(globs)]
+
+extern crate piston;
+extern crate graphics;
+extern crate sdl2_game_window;
+extern crate opengl_graphics;
+
+use opengl_graphics::{Gl, Texture};
+use Window = sdl2_game_window::GameWindowSDL2;
+use graphics::*;
+use piston::{
+    AssetStore,
+    GameIterator,
+    GameIteratorSettings,
+    GameWindowSettings,
+    Render,
+};
+
+#[start]
+fn start(argc: int, argv: **u8) -> int {
+    // Run on the main thread.
+    native::start(argc, argv, main)
+}
+
+fn main() {
+    let mut window = Window::new(
+        GameWindowSettings {
+            title: "Rust-Graphics-Lab: Texture App".to_string(),
+            size: [600, 300],
+            fullscreen: false,
+            exit_on_esc: true,
+        }
+    );
+
+    let asset_store = AssetStore::from_folder("assets");
+
+    let image = asset_store.path("dices.png").unwrap();
+    let image = Texture::from_path(&image).unwrap();
+
+    let game_iter_settings = GameIteratorSettings {
+        updates_per_second: 120,
+        max_frames_per_second: 60,
+    };
+    let mut game_iter = GameIterator::new(&mut window, &game_iter_settings);
+    let ref mut gl = Gl::new();
+    loop {
+        match game_iter.next() {
+            None => break,
+            Some(mut e) => match e {
+                Render(ref mut args) => {
+                    gl.viewport(0, 0, args.width as i32, args.height as i32);
+                    let c = Context::abs(
+                            args.width as f64,
+                            args.height as f64
+                        );
+                    c.rgb(1.0, 1.0, 1.0).draw(gl);
+                    c.rect(0.0, 0.0, 50.0, 50.0).rgb(1.0, 0.0, 0.0).draw(gl);
+                    let offset = 150.0;
+                    // c.trans(0.0, offset).image(&image).draw(gl);
+                },
+                _ => {},
+            }
+        }
+    }
+}
+
